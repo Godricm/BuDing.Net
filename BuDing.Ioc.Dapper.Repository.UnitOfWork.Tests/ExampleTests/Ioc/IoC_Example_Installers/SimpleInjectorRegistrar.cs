463192e8cc35cb94d3c4d24139f07831942f6b48
Godric <miaochenGod@outlook.com>
2019-06-03 17:22:57 +0800

    [add] SimpleInjector的Ioc单元测试

---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/IoC_Example_Installers/SimpleInjectorRegistrar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+using SimpleInjector;
+using SimpleInjector.Diagnostics;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers
+{
+    [NoIoCFluentRegistration]
+    public class SimpleInjectorRegistrar
+    {
+        public void Register(Container container)
+        {
+            container.RegisterInstance<IDbFactory>(new SimpleInjectorDbFactory(container));
+        }
+        public static void RegisterDisposableTransient(Container container, Type service, Type implementation)
+        {
+            var reg = Lifestyle.Transient.CreateRegistration(implementation, container);
+            reg.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "suppressed.");
+            container.AddRegistration(service, reg);
+        }
+
+
+        [NoIoCFluentRegistration]
+        sealed class SimpleInjectorDbFactory:IDbFactory
+        {
+            private readonly Container _container;
+
+            public SimpleInjectorDbFactory(Container container)
+            {
+                _container = container;
+            }
+
+            public T Create<T>() where T : class, ISession
+            {
+                return _container.GetInstance<T>();
+            }
+
+            public T CreateSession<T>() where T : class, ISession
+            {
+                return _container.GetInstance<T>();
+            }
+            public TUnitOfWork Create<TUnitOfWork, TSession>(IsolationLevel isolationLevel = IsolationLevel.Serializable) where TUnitOfWork : class, IUnitOfWork where TSession : class, ISession
+            {
+                return new BuDing.Ioc.UnitOfWork.UnitOfWork(_container.GetInstance<IDbFactory>(), Create<TSession>(),
+                    isolationLevel, true) as TUnitOfWork;
+            }
+            public T Create<T>(IDbFactory factory, ISession session, IsolationLevel isolationLevel = IsolationLevel.Serializable) where T : class, IUnitOfWork
+            {
+                return new BuDing.Ioc.UnitOfWork.UnitOfWork(factory, session, isolationLevel) as T;
+            }
+            public void Release(IDisposable instance)
+            {
+                instance?.Dispose();
+            }
+        }
+    }
+}
---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/SimpleInjectorTests.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Repository;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+using NUnit.Framework;
+using SimpleInjector;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc
+{
+    [TestFixture]
+    public class SimpleInjectorTests
+    {
+        private static Container _container;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            if (_container == null)
+            {
+                _container = new Container();
+                Assert.DoesNotThrow(() =>
+                {
+                    new SimpleInjectorRegistrar().Register(_container);
+                    var registrations =
+                        (from type in Assembly.GetExecutingAssembly().GetTypes()
+                            where !type.GetCustomAttributes(true).Any(x => x.GetType() == typeof(NoIoCFluentRegistration))
+                            && !type.IsAbstract && !type.IsInterface
+                            && type.GetInterfaces().Any(x=>x!=typeof(IDisposable))
+                                select new { Services = type.GetInterfaces(), Implementation = type }).ToArray();
+
+                    foreach (var reg in registrations)
+                    {
+                        foreach (var service in reg.Services)
+                        {
+                            if (string.CompareOrdinal(service.Name.Substring(1), reg.Implementation.Name) == 0)
+                            {
+                                if (reg.Implementation.GetInterfaces().Any(x => x == typeof(IDisposable)))
+                                {
+                                    SimpleInjectorRegistrar.RegisterDisposableTransient(_container, service, reg.Implementation);
+                                    continue;
+                                }
+                                try
+                                {
+                                    _container.Register(service, reg.Implementation, Lifestyle.Transient);
+                                }
+                                catch (Exception)
+                                {
+                                }
+                                
+                            }
+                        }
+                    }
+                    _container.Verify();
+                });
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_1_Resolves_ISession()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            ITestSession session = null;
+            Assert.DoesNotThrow(() => session = dbFactory.Create<ITestSession>());
+            Assert.That(session, Is.Not.Null);
+        }
+
+
+        [Test, Category("Integration")]
+        public static void Install_2_Resolves_IUnitOfWork()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            using (var session = dbFactory.Create<ITestSession>())
+            {
+                IUnitOfWork uow = null;
+                Assert.DoesNotThrow(()=> uow = session.UnitOfWork(IsolationLevel.Serializable));
+                Assert.That(uow, Is.Not.Null);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_5_Resolves_WithCorrectConnectionString()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSession>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Tests.db;Version=3;New=True;BinaryGUID=False;"), Is.True);
+            }
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSessionMemory>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Data Source=:memory:;Version=3;New=True;"), Is.True);
+            }
+        }
+
+
+        [Test, Category("Integration")]
+        public static void Install_99_Resolves_IBravoRepository()
+        {
+            IBraveRepository repo = null;
+            Assert.DoesNotThrow(() => repo = _container.GetInstance<IBraveRepository>());
+            Assert.That(repo, Is.Not.Null);
+        }
+    }
+}
