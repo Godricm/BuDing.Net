c61bcf30b4785f80bf33018275e625b657c0a97f
Godric <miaochengod@outlook.com>
2019-06-03 20:06:14 +0800

    [update] 实现Rollback单元测试

--- BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/SpecialTests/DbTransactionTests.cs
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/SpecialTests/DbTransactionTests.cs
@@ -1,7 +1,10 @@
-﻿using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+﻿using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Repository;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using FluentAssertions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.SpecialTests
@@ -12,7 +15,15 @@ namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.SpecialTests
         [Test]
         public void Rollback_DoesNotThrow_OnDisposalAfterAlreadyBeingCalled()
         {
-           
+            var repo = new BraveRepository(Factory);
+            Assert.DoesNotThrow(() => {
+                using(var uow = Connection.UnitOfWork(IsolationLevel.Serializable))
+                {
+                    var result = repo.SaveOrUpdate(new Brave { NewId = 3 }, uow);
+                    result.Should().BePositive();
+                    uow.Rollback();
+                }
+            });
         }
     }
 }
