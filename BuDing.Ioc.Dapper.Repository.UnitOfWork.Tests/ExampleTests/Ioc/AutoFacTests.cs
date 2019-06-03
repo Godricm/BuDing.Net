1ce748c3eb42daf4fef5a04f48aa7a2a38215c2d
Godric <miaochenGod@outlook.com>
2019-06-03 17:21:44 +0800

    [add] AutoFac的Ioc单元测试

---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/AutoFacTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Repository;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+using NUnit.Framework;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc
+{
+    [TestFixture]
+    public class AutoFacTests
+    {
+        private static IContainer _container;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            if (_container == null)
+            {
+                var builder = new ContainerBuilder();
+                Assert.DoesNotThrow(() =>
+                {
+                    new AutofacRegistrar().Register(builder);
+                    builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces()
+                        .Where(t => t.GetInterfaces().Any(i => i != typeof(IDisposable)) &&
+                                    t.GetCustomAttribute<NoIoCFluentRegistration>() == null);
+                    _container = builder.Build();
+                });
+                Assert.That(_container.IsRegistered<ITestSession>(), Is.True);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_1_Resolves_ISession()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            ITestSession session = null;
+            Assert.DoesNotThrow(() => session = dbFactory.Create<ITestSession>());
+            Assert.That(session, Is.Not.Null);
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_2_Resolves_IUnitOfWork()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var session = dbFactory.Create<ITestSession>())
+            {
+                IUnitOfWork uow = null;
+                Assert.DoesNotThrow(() => uow = session.UnitOfWork(IsolationLevel.Serializable));
+                Assert.That(uow, Is.Not.Null);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_5_Resolves_WithCorrectConnectionString()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSession>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Tests.db;Version=3;New=True;BinaryGUID=False;"), Is.True);
+            }
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSessionMemory>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Data Source=:memory:;Version=3;New=True;"), Is.True);
+            }
+        }
+
+
+        [Test, Category("Integration")]
+        public static void Install_99_Resolves_IBravoRepository()
+        {
+            IBraveRepository repo = null;
+            Assert.DoesNotThrow(() => repo = _container.Resolve<IBraveRepository>());
+            Assert.That(repo, Is.Not.Null);
+        }
+    }
+}
---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/IoC_Example_Installers/AutofacRegistrar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Autofac;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers
+{
+    [NoIoCFluentRegistration]
+    public class AutofacRegistrar:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            Register(builder);
+        }
+
+        public void Register(ContainerBuilder builder)
+        {
+            builder.Register(c => new DbFactory(c.Resolve<IComponentContext>())).As<IDbFactory>().SingleInstance();
+            builder.RegisterGeneric(typeof(Repository<,>)).As(typeof(IRepository<,>));
+            builder.RegisterType<BuDing.Ioc.UnitOfWork.UnitOfWork>().As<IUnitOfWork>();
+            //ToDo something like this to inject IRepository interfaces without a named interface
+            //var assemblies = AssemblyHelper.GetReferencingAssemblies(AssemblyHelper.ProjectName);
+            // builder.RegisterAssemblyTypes(assemblies.ToArray()).AsClosedTypesOf(typeof(IRepository<,>));
+        }
+
+        [NoIoCFluentRegistration]
+        sealed class  DbFactory:IDbFactory
+        {
+            private readonly IComponentContext _container;
+
+            public DbFactory(IComponentContext container)
+            {
+                _container = container;
+            }
+
+
+            public T Create<T>() where T : class, ISession
+            {
+                return _container.Resolve<T>();
+            }
+
+            public TUnitOfWork Create<TUnitOfWork, TSession>(IsolationLevel isolationLevel = IsolationLevel.RepeatableRead) where TUnitOfWork : class, IUnitOfWork where TSession : class, ISession
+            {
+                return _container.Resolve<TUnitOfWork>(new NamedParameter("factory", _container.Resolve<IDbFactory>()),
+                    new NamedParameter("session", Create<TSession>()), new NamedParameter("isolationLevel", isolationLevel)
+                    , new NamedParameter("sessionOnlyForThisUnitOfWork", true));
+            }
+
+            public T Create<T>(IDbFactory factory, ISession session, IsolationLevel isolationLevel = IsolationLevel.RepeatableRead) where T : class, IUnitOfWork
+            {
+                return _container.Resolve<T>(new NamedParameter("factory", factory),
+                    new NamedParameter("session", session), new NamedParameter("isolationLevel", isolationLevel));
+            }
+
+            public void Release(IDisposable instance)
+            {
+                instance.Dispose();
+            }
+        }
+    }
+}
