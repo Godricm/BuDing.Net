6685b75bf19182f51eb244fd25d16207d174a4af
Godric <miaochenGod@outlook.com>
2019-06-03 17:24:49 +0800

    [add] Unity的Ioc单元测试

---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/IoC_Example_Installers/UnityRegistrar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+using Unity.Resolution;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers
+{
+    [NoIoCFluentRegistration]
+    public class UnityRegistrar
+    {
+        public void Register(IUnityContainer container)
+        {
+            container.RegisterType<IDbFactory, UnityDbFactory>(new ContainerControlledLifetimeManager(),
+                new InjectionConstructor(container));
+            container.RegisterType<IUnitOfWork, BuDing.Ioc.UnitOfWork.UnitOfWork>();
+        }
+        [NoIoCFluentRegistration]
+        private sealed class  UnityDbFactory:IDbFactory
+        {
+            private readonly IUnityContainer _container;
+
+            public UnityDbFactory(IUnityContainer container)
+            {
+                _container = container;
+            }
+
+            public  T Create<T>() where T : class,ISession
+            {
+                return _container.Resolve<T>();
+            }
+
+            public TUnitOfWork Create<TUnitOfWork, TSession>(
+                IsolationLevel isolationLevel = IsolationLevel.Serializable) where TUnitOfWork : class, IUnitOfWork
+                where TSession : class, ISession
+            {
+                return _container.Resolve<TUnitOfWork>(
+                    new ParameterOverride("factory", _container.Resolve<IDbFactory>()),
+                    new ParameterOverride("session", Create<TSession>()),
+                    new ParameterOverride("isolationLevel", isolationLevel),
+                    new ParameterOverride("sessionOnlyForThisUnitOfWork", true));
+            }
+
+            public T Create<T>(IDbFactory factory, ISession session,
+                IsolationLevel isolationLevel = IsolationLevel.Serializable) where T : class, IUnitOfWork
+            {
+                return _container.Resolve<T>(new ParameterOverride("factory", factory),
+                    new ParameterOverride("session", session), new ParameterOverride("isolationLevel", isolationLevel),
+                    new ParameterOverride("sessionOnlyForThisUnitOfWork", false));
+            }
+
+            public void Release(IDisposable instance)
+            {
+                instance.Dispose();
+            }
+        }
+    }
+
+}
---
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/ExampleTests/Ioc/UnityTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc.IoC_Example_Installers;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Repository;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers;
+using BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.TestHelpers.Registrations;
+using BuDing.Ioc.UnitOfWork.Interfaces;
+using NUnit.Framework;
+using Unity;
+
+namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.ExampleTests.Ioc
+{
+    [TestFixture]
+    public class UnityTests
+    {
+        private static IUnityContainer _container;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            if (_container == null)
+            {
+                _container = new UnityContainer();
+                Assert.DoesNotThrow(() =>
+                {
+                    new UnityRegistrar().Register(_container);
+                    new UnitConventionRegistrar(_container);
+                });
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_1_Resolves_ISession()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            ITestSession session = null;
+            Assert.DoesNotThrow(() => session = dbFactory.Create<ITestSession>());
+            Assert.That(session, Is.Not.Null);
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_2_Resolves_IUnitOfWork()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var session = dbFactory.Create<ITestSession>())
+            {
+                IUnitOfWork uow = null;
+                Assert.DoesNotThrow(() => uow = session.UnitOfWork(IsolationLevel.Serializable));
+                Assert.That(uow, Is.Not.Null);
+            }
+        }
+        [Test, Category("Integration")]
+        public static void Install_5_Resolves_withCorrectConnectionString()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSession>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Tests.db;Version=3;New=True;BinaryGUID=False;"), Is.True);
+            }
+
+            using (var uow = dbFactory.Create<IUnitOfWork, ITestSessionMemory>(IsolationLevel.Serializable))
+            {
+                Assert.That(uow.Connection.State, Is.EqualTo(ConnectionState.Open));
+                Assert.That(uow.Connection.ConnectionString.EndsWith("Data Source=:memory:;Version=3;New=True;"), Is.True);
+            }
+        }
+        [Test, Category("Integration")]
+        public static void Install_99_Resolves_IBravoRepository()
+        {
+            IBraveRepository repo = null;
+            Assert.DoesNotThrow(() => repo = _container.Resolve<IBraveRepository>());
+            Assert.That(repo, Is.Not.Null);
+        }
+    }
+}
