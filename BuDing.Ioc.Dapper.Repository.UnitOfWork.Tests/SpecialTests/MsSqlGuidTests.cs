21b44251d87e4b45c956bb0c2459a8b5cc99e15e
godric <miaochenGod@outlook.com>
2019-06-04 15:34:08 +0800

    [add] 新增MsSqlGuidTests单元测试

--- BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/SpecialTests/MsSqlGuidTests.cs
+++ BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests/SpecialTests/MsSqlGuidTests.cs
@@ -1,7 +1,10 @@
 ﻿using BuDing.Core.Entity;
 using BuDing.Ioc.UnitOfWork.Abstractions;
 using BuDing.Ioc.UnitOfWork.Interfaces;
+using Dapper;
+using Dapper.FastCrud;
 using FakeItEasy;
+using FluentAssertions;
 using NUnit.Framework;
 using SimpleMigrations;
 using SimpleMigrations.DatabaseProvider;
@@ -11,6 +14,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -33,12 +37,46 @@ namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.SpecialTests
             migrator.MigrateToLatest();
         }
 
-        [Test,Category("IntegrationMssqlce"),Explicit("Needs Sql")]
+        [Test, Category("IntegrationMssqlce"), Explicit("Needs Sql")]
         public void Insert_Returns_IdAsGuid()
         {
-            
+            var foo = new FooGuidTest { Something = "bar 1" };
+            TestSession.Execute("DELETE FROM FooGuidTest");
+            TestSession.Insert(foo);
+            var actual = TestSession.Find<FooGuidTest>();
+            actual.Should().HaveCount(x => x > 0);
+            actual.First().Id.Should().NotBe(new Guid());
         }
 
+        [Test, Category("IntegrationMssqlce"), Explicit("Needs Sql")]
+        public void SaveAndUpdate_Returns_AsGuid()
+        {
+            var foo = new FooGuidTest { Something = "bar 1" };
+            TestSession.Execute("Delete From FooGuidTest");
+            var repo = new FooRepo1(A.Fake<IDbFactory>());
+            using (var uow = new Ioc.UnitOfWork.UnitOfWork(A.Fake<IDbFactory>(), TestSession))
+            {
+                repo.SaveOrUpdate(foo, uow);
+            }
+            var actual = repo.GetAll(TestSession);
+            actual.Should().HaveCount(x => x > 0);
+            actual.First().Id.Should().NotBe(new Guid());
+        }
+
+        [Test, Category("IntegrationMssqlce"), Explicit("Needs Sql")]
+        public void SaveAndUpdate_Returns_dAsGuidWhereEntityIsIEntity()
+        {
+            var foo = new FooGuidTestWithIEntity { Something = "bar 1" };
+            TestSession.Execute("DELETE FROM FooGuidTestWithIEntity");
+            var repo = new FooRepo2(A.Fake<IDbFactory>());
+            using(var uow=new Ioc.UnitOfWork.UnitOfWork(A.Fake<IDbFactory>(), TestSession))
+            {
+                repo.SaveOrUpdate(foo, uow);
+            }
+            var actual = repo.GetAll(TestSession);
+            actual.Should().HaveCount(x => x > 0);
+            actual.First().Id.Should().NotBe(new Guid()); 
+        }
 
         [Table("FooGuidTest")]
         class FooGuidTest
@@ -50,6 +88,28 @@ namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.SpecialTests
             public string Something { get; set; }
         }
 
+        class CreateFoo : Migration
+        {
+            protected override void Down()
+            {
+                Execute("DROP TABLE FooGuidTest");
+            }
+
+            protected override void Up()
+            {
+                if (!Connection.ConnectionString.Contains(DbName)) return;
+                Execute(@"CREATE TABLE FooGuidTest(Id UNIQUEIDENTIFIER DEFAULT NEWID(),Something VARCHAR(20));");
+                Execute(@"CREATE TABLE FooGuidTestWithIEntity(Id UNIQUEIDENTIFIER DEFAULT NEWID(),Something VARCHAR(20));");
+            }
+        }
+
+        class FooRepo1 : Repository<FooGuidTest, Guid>, IRepository<FooGuidTest, Guid>
+        {
+            public FooRepo1(IDbFactory factory) : base(factory)
+            {
+            }
+        }
+
         class FooGuidTestWithIEntity : IEntity<Guid>
         {
             [Key]
@@ -59,6 +119,12 @@ namespace BuDing.Ioc.Dapper.Repository.UnitOfWork.Tests.SpecialTests
             public string Something { get; set; }
         }
 
+        class FooRepo2 : Repository<FooGuidTestWithIEntity, Guid>, IRepository<FooGuidTestWithIEntity, Guid>
+        {
+            public FooRepo2(IDbFactory factory) : base(factory)
+            {
+            }
+        }
 
         private class TestSqlForGuid : Session<SqlConnection>, ITestSqlForGuid
         {
