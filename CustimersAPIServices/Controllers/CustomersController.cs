7aeb9e61140879c2b0fe1ec22f56379ddef3f96a
Godric <miaochengod@outlook.com>
2019-06-06 22:23:01 +0800

    [add] 新增BasicGateway，并通过验证

---
+++ CustimersAPIServices/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustimersAPIServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "cherry", "godric" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"cherry_{id}";
+        }
+    }
+}
