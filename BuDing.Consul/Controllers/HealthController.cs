b1f268b35aff652fa1a15b1a9f83d0d9d23d2728
Godric <miaochenGod@outlook.com>
2019-06-07 15:09:11 +0800

    [add] 新增consul项目，目的是实现服务治理，后续与ocelot集成

---
+++ BuDing.Consul/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuDing.Consul.Controllers
+{
+    /// <summary>
+    /// 健康检查
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("ok");
+        }
+    }
+}
