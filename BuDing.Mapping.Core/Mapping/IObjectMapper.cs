858bed1efd264e14e51be681d364164e133f4aab
Godric <miaochenGod@outlook.com>
2019-06-12 17:22:36 +0800

    [add] mapping

---
+++ BuDing.Mapping.Core/Mapping/IObjectMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuDing.Mapping
+{
+    public interface IObjectMapper
+    {
+        TDestination Map<TDestination>(object source);
+
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
